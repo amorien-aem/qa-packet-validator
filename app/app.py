e32775265481b116f4a0b6fd02fcd36bf48a8c09
Adam Morien <adam.morien@aeminc.com>
2025-10-07 00:16:23 +0000

    commit

--- app/app.py
+++ app/app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+
 app = Flask(__name__)
-@app.route('/')
+app.secret_key = 'supersecretkey'  # Needed for flashing messages
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET'])
 def index():
-    return 'QA Packet Validator Web App'
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(url_for('index'))
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        flash('File uploaded successfully!')
+        return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
